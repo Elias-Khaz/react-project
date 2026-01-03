81dbb2d7cb63e0bc1a3ac06467ac388a534a79e5
Elias-Khaz <elias.khazroun5@gmail.com>
2026-01-03 17:33:58 +0100

    added preview button and styling

--- react-project/src/pages/portfolio.jsx
+++ react-project/src/pages/portfolio.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+
 import ProjectOne from "../components/projectOne";
 import ProjectTwo from "../components/projectTwo";
 import ProjectThree from "../components/projectThree";
 
 function Portfolio() {
+  const [showProjects, setShowProjects] = useState(false);
+
   return (
     <div>
       <h1>My Projects</h1>
 
-      <ProjectOne />
-      <ProjectTwo />
-      <ProjectThree />
+      <button onClick={() => setShowProjects(!showProjects)}>
+        {showProjects ? "Hide Projects" : "Preview Projects"}
+      </button>
+
+      {showProjects && (
+        <>
+          <ProjectOne />
+          <ProjectTwo />
+          <ProjectThree />
+        </>
+      )}
     </div>
   );
 }
